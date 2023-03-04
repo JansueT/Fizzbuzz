95fb61efa288c024bdb17390b36d60a7f4abe163
Cansu T <111116267+JansueT@users.noreply.github.com>
2023-03-04 12:58:45 +0300

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,5 @@
+//JansueT solution
+
 module.exports = function () {
   // write your code here
   function FizzBuzz(arr) {
@@ -17,3 +19,24 @@ module.exports = function () {
   }
   return Array;
 };
+
+// Mussie's line of code.
+
+function fizzBuzz() {
+  let arr = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      arr.push("Fizz");
+    } else if (i % 5 === 0) {
+      arr.push("Buzz");
+    } else {
+      arr.push(i);
+    }
+  }
+  return arr;
+}
+
+console.log(fizzBuzz());
+
